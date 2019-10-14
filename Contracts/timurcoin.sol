4b9cecc8a07e0bc293c1d82546252db67f2fdcb9
Timur Ercan <timur.ercan31@gmail.com>
2019-10-14 07:35:21 +0200

    qoc

--- Contracts/timurcoin.sol
+++ Contracts/timurcoin.sol
@@ -1,34 +1,32 @@
-pragma solidity ^0.5.11;contract TimurCoin { // set contract name to token name
+pragma solidity ^0.5.11;contract TimurCoin {
     string public name;
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
 
-    // Balances for each account
     mapping(address => uint256) balances;address devAddress;// Events
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    // This is the constructor and automatically runs when the smart contract is uploaded
-    // Owner of account approves the transfer of an amount to another account
     mapping(address => mapping (address => uint256)) allowed;
-    function tokenName() public { // Set the constructor to the same name as the contract name
-        name = "timurCoin"; // set the token name here
-        symbol = "timc"; // set the Symbol here
-        decimals = 18; // set the number of decimals
-        devAddress = 0xc9BB80Bd7C4676CA98292826c8ED71fb08dbcE36; // Add the address that you will distribute tokens from here
-        // TODO distribute by stake in some way
-        // Distribute by stake on transaction => incentivize usage? x % of transaction or absolut
-        uint initialBalance = 1000000000000000000*100000; // 1M tokens
+
+    constructor() public {
+        name = "timurCoin";
+        symbol = "timc";
+        decimals = 18;
+        devAddress = 0xc9BB80Bd7C4676CA98292826c8ED71fb08dbcE36;
+        // TODO distribute coins regularly by stake in some way
+        // Distribute "cashback" on transaction as incentivize for usage? relative or absolut of transaction volume
+        uint initialTokenCount = 100000;
+        uint initialBalance = 1000000000000000000 * initialTokenCount;
         balances[devAddress] = initialBalance;
-        totalSupply += initialBalance; // Set the total suppy
+        totalSupply += initialBalance;
     }
 
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
-    // Transfer the balance from owner's account to another account
     function transfer(address _to, uint256 _amount) public returns (bool success) {
         if (balances[msg.sender] >= _amount &&
             _amount > 0 &&
@@ -59,8 +57,6 @@ pragma solidity ^0.5.11;contract TimurCoin { // set contract name to token name
         }
     }
 
-    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-    // If this function is called again it overwrites the current allowance with _value.
     function approve(address _spender, uint256 _amount) public returns (bool success) {
         allowed[msg.sender][_spender] = _amount;
         emit Approval(msg.sender, _spender, _amount);
